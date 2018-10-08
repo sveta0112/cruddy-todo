f54a226fd30a09ae6abb39a1d900a3a3f6cec6c1
sveta0112 <sveta0112@hotmail.com>
2018-10-08 16:14:35 -0700

    updateFile function is passing

--- datastore/index.js
+++ datastore/index.js
@@ -43,13 +43,15 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  fs.readFile(exports.dataDir + `/${id}.txt`, 'utf8', function (err, data) {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(exports.dataDir + `/${id}.txt`, text, function() {
+        callback(null, { id, text });
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
