3bfd7052b072200d61b85b01f541b143d30e8058
sveta0112 <sveta0112@hotmail.com>
2018-10-08 15:10:55 -0700

    passing create function

--- datastore/index.js
+++ datastore/index.js
@@ -8,9 +8,14 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId(function (err, id) {
+    fs.writeFile(exports.dataDir + `/${id}.txt`, text, function(err) {
+      if (err) {
+        throw err;
+      } 
+      callback(null, { id, text });
+    });
+  });
 };
 
 exports.readAll = (callback) => {
