c1c211d901deb27cd6d032c984ca0a33ff931fd8
Hack Reactor Student <student@hackreactor.com>
2018-10-08 14:52:06 -0700

    getUniqIndex function is passing

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,16 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter(function(err, counter) {
+    if (err) {
+      null;
+    } else {
+      writeCounter(counter + 1, function(err, counterString) {
+        callback(err, counterString);
+      });
+    }
+  });
 };
 
 
